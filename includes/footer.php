e1b9cdfd410d23c9f3ce74fb5dc59ae01c5b3a72
asm2305 <43830851+asm2305@users.noreply.github.com>
2025-05-18 18:46:32 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+       </main>
+
+    <footer class="custom-footer text-white py-4 mt-5">
+        <div class="container">
+            <div class="row">
+               
+			    <div class="col-md-4">
+                    <center><h5>تواصل</h5>
+                    <p><i class="fas fa-phone me-2"></i> +966 565261868</p>
+                    <p><i class="fas fa-envelope me-2"></i> savelife@savelife.com</p>
+              </center>  </div>
+			   
+                <div class="col-md-4">
+                    <center><h5>روابط تهمك</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="donors/register.php" class="text-white">كن متبرع</a></li>
+                        <li><a href="\SAVELIFEnew\patients/search.php" class="text-white">البحث عن متبرعين</a></li>
+                        <li><a href="\SAVELIFEnew\centers/map.php" class="text-white">مراكز التبرع</a></li>
+                    </ul>
+               </center> </div>
+				 <div class="col-md-4">
+                   <center> <h5>من نحن</h5>
+                    <p>نظام إدارة التبرع بالدم لدينا هو جسر أمل بين من يمنح الحياة ومن يحتاج إليها
+نسعى إلى بناء مجتمع متكامل من المتبرعين والمتطوعين والمراكز الصحية، لنجعل عملية التبرع أكثر سهولة، أماناً، وانتشاراً
+انضم إلينا... وساعد في رسم ابتسامة جديدة على وجه كل مريض يحتاج إلى يد العون</p>
+               </center> </div>
+				
+            </div>
+            <hr>
+            <div class="text-center">
+                <p class="mb-0">&copy; <?= date('Y') ?> Blood Donation Management System. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
